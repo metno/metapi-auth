fa83249f6d528d4de56715cf55ffd8f98737c6fd
Michael Akinde <michael.akinde@met.no>
2016-09-28 15:13:25 +0200

    Moved authorization controller to an own package.

--- app/controllers/auth/AuthorizationController.scala
+++ app/controllers/auth/AuthorizationController.scala
@@ -22,7 +22,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
     MA 02110-1301, USA
 */
-package controllers
+package controllers.auth
 
 import javax.inject.Inject
 import play.api.Play.current
